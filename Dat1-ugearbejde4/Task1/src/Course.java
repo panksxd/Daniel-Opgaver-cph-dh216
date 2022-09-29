0396c7c62131d47b4f23d59e4c2a048642bf6f3d
panksxd <panksxd@gmail.com>
2022-09-29 17:47:20 +0200

    jaja

---
+++ Dat1-ugearbejde4/Task1/src/Course.java
@@ -0,0 +1,14 @@
+public class Course
+{
+   private String name;
+
+   Course(String name)
+   {
+       this.name = name;
+   }
+
+   public String toString()
+   {
+       return "Course: " + name;
+   }
+}
---
+++ Dat1-ugearbejde4/Task1/src/Student.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Student
+{
+    private String name;
+    private ArrayList<Course> courses = new ArrayList<>();
+
+    Student(String name)
+    {
+        this.name = name;
+    }
+
+    public void addCourse(Course a)
+    {
+        courses.add(a);
+    }
+
+    public String toString()
+    {
+       return "Student: "+name + "is involved with the following courses: "+ courses;
+    }
+}
